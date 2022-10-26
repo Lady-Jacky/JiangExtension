1f786ad06f29b59492ea841f8fe23a394d25c51f
1E10 Laptop <sample@bths.edu>
2022-10-26 11:02:47 -0400

    Step 1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public Calculator(){}
+
+    public double calcSolve(String expression) {
+        if (expression.indexOf("*") != -1) {
+            int index = expression.indexOf("*");
+        } else if (expression.indexOf("/") != -1)
+    }
+
 }
